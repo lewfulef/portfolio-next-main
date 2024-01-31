7c2fcad1173cc67d6a63e5d5220a53aef331044f
Lewfu Lef <l.huichalafb@gmail.com>
2024-01-31 20:09:25 -0300

    project component updated

--- app/projects/page.js
+++ app/projects/page.js
@@ -7,7 +7,7 @@ const projects = () => {
     <div className="grid grid-cols-4 h-screen items-center">
       <div className="flex flex-col justify-center items-center">
         
-        <h3 className="text-3xl font-bold text-black">Tu texto aquí</h3>
+        <h4 className="text-2xl mb-5 font-light text-black">Landing de negocio</h4>
         <Image
           width={300}
           height={300}
@@ -15,11 +15,12 @@ const projects = () => {
           src="https://res.cloudinary.com/dc1w1o2nl/image/upload/v1706532865/Screen_Shot_2024-01-29_at_09.52.21_m3juxe.png"
         />
         <div className="flex gap-5 mt-5">
-          <Button color="default" variant="faded">repositorio</Button>
-          <Button color="default" variant="ghost">deploy</Button>
+          <a href="https://github.com/lewfulef/landing-de-negocio/"><Button color="default" variant="faded">Ver repositorio</Button></a>
+          <a href="https://lewfulef.github.io/landing-de-negocio/"><Button color="default" variant="ghost">Ver despliegue</Button></a>
         </div>
       </div>
       <div className="flex flex-col justify-center items-center">
+      <h4 className="text-2xl mb-5 font-light text-black">CRUD app</h4>
         <Image
           height={300}
           width={300}
@@ -27,11 +28,12 @@ const projects = () => {
           src="https://res.cloudinary.com/dc1w1o2nl/image/upload/v1706626295/Screen_Shot_2024-01-30_at_11.49.11_pluibh.png"
         />
         <div className="flex gap-5 mt-5">
-          <Button color="primary">repositorio</Button>
-          <Button color="secondary">deploy</Button>
+          <a href="https://github.com/lewfulef/aplicacionCRUD"><Button color="default" variant="faded">Ver repositorio</Button></a>
+          <a href="https://lewfulef.github.io/aplicacionCRUD/"><Button color="default" variant="ghost">Ver despliegue</Button></a>
         </div>
       </div>
       <div className="flex flex-col justify-center items-center">
+      <h4 className="text-2xl mb-5 font-light text-black">Restaurant App</h4>
         <Image
           height={300}
           width={300}
@@ -39,11 +41,12 @@ const projects = () => {
           src="https://res.cloudinary.com/dc1w1o2nl/image/upload/v1706626358/Screen_Shot_2024-01-30_at_11.50.10_o40ayt.png"
         />
         <div className="flex gap-5 mt-5">
-          <Button color="primary">repositorio</Button>
-          <Button color="secondary">deploy</Button>
+          <a href="https://github.com/lewfulef/restaurant-app-dg/"><Button color="default" variant="faded">Ver repositorio</Button></a>
+          <a href="https://lewfulef.github.io/restaurant-app-dg/"><Button color="default" variant="ghost">Ver despliegue</Button></a>
         </div>
       </div>
       <div className="flex flex-col justify-center items-center">
+      <h4 className="text-2xl mb-5 font-light text-black">E-commerce</h4>
         <Image
           height={300}
           width={300}
@@ -51,8 +54,8 @@ const projects = () => {
           src="https://res.cloudinary.com/dc1w1o2nl/image/upload/v1706626389/Screen_Shot_2024-01-30_at_11.49.38_kvfolc.png"
         />
         <div className="flex gap-5 mt-5">
-          <Button color="primary">repositorio</Button>
-          <Button color="secondary">deploy</Button>
+          <a href="https://github.com/lewfulef/client/"><Button color="default" variant="faded">Ver repositorio</Button></a>
+          <a href="https://lewfulef.github.io/client/"><Button color="default" variant="ghost">Ver despliegue</Button></a>
         </div>
       </div>
     </div>
